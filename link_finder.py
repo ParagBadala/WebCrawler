172bfd56c7a79fa4adfb7fd1d6172dba521f6d8f
Parag Badala <parag.badala@gmail.com>
2017-06-27 20:31:12 +0530

    WebCrawler

---
+++ link_finder.py
@@ -0,0 +1,25 @@
+from HTMLParser import HTMLParser
+from urlparse2 import urlparse2
+
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self, base_url, page_url):
+        super(LinkFinder,self).__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for(attribute, value) in attrs:
+                if attribute == 'href':
+                    url = urlparse2.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+    def page_links(self):
+        return self.links
+
+    def error(self, message):
+        pass
+
